f1ed2f1526284bc05adde9104301b14b490107e8
Martin Kleinhans <kleinhans.martin@bcg.com>
2025-02-01 16:00:38 +0100

    added coordinator instance

---
+++ VouteCoordinator/module.php
@@ -0,0 +1,24 @@
+<?php
+
+class VouteCoordinator extends IPSModule
+{
+    public function Create()
+    {
+         // Never delete this line!
+         parent::Create();
+    }
+
+    public function ApplyChanges() {
+        parent::ApplyChanges();
+    }
+
+    public function RequestAction($Ident, $Value)
+    {
+    }
+
+    //------------------------------------------------
+    // Public methods
+
+    //------------------------------------------------
+    // private methods
+}
